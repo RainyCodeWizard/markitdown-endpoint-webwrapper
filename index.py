51660e4edc8ce8de9facb8d2d3986f94817deead
Jonathan-Gill-Moss <jonathan@gill-moss.co.uk>
2025-04-20 13:01:46 +0100

    minor update to git ignore to remove old data,   changed debug port to 8080

--- index.py
+++ index.py
@@ -81,4 +81,4 @@ async def convert_markdown(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("index:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("index:app", host="0.0.0.0", port=8080, reload=True)
